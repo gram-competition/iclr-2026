24d71825f79bd2b8c8c295918404a3256c421cb7
Julian Suk <julian.suk@tum.de>
2026-05-28 19:14:31 +0200

    chore(models): clean up branch before merge

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,2 @@
-# Lightweight exports for the challenge submission interface.
-# Training and evaluation scripts still import subpackages directly.
-
 from .mlp import MLP
-from .aero_chrono_mixer import AeroChronoMixer
 from .smoothsplatnet import SmoothSplatNet
-
-__all__ = ["MLP", "AeroChronoMixer", "SmoothSplatNet"]
